dafedc114cc67622a02cfcdce84832cbe7fbffa9
Ethan Stein <ethanmstn33@gmail.com>
2024-03-08 17:11:45 -0500

    Update binary files and make changes to UIElements in the code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,20 +5,40 @@ public class EnemyController : MonoBehaviour
     public int maxHealth = 100; // Maximum health of the enemy
     public int currentHealth; // Current health of the enemy
     public MoneyHUD MoneyHUD;
+    public BoxCollider[] rooms;
+    public AudioSource krabsWalk;
+
+    Vector3 destination;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        destination = findRandomFloorPositionInRoom(rooms, transform.position.y);
+        gameObject.transform.position = destination;
+
         currentHealth = maxHealth; // Set current health to max health when the enemy starts
         if (MoneyHUD == null)
         {
             // Try to find the MoneyHUD component in the scene if not assigned
             MoneyHUD = FindObjectOfType<MoneyHUD>();
         }
+        
+        krabsWalk.Play();
     }
 
     public void Update()
     {
+
+        if (Vector3.Distance(transform.position, destination) < 0.1)
+        {
+            destination = findRandomFloorPositionInRoom(rooms, transform.position.y);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, 0.1f);
+        }
+
         TakeDamage();
     }
     // Method to handle taking damage
@@ -61,5 +81,28 @@ public class EnemyController : MonoBehaviour
             // Destroy the bullet
             Destroy(other.gameObject);
         }
+    }
+
+    Vector3 findRandomFloorPositionInRoom (BoxCollider[] rooms, float y) {
+
+        BoxCollider room = rooms[Random.Range(0, rooms.Length)];
+
+        Vector3 pos = room.transform.position;
+        Vector3 size = room.GetComponent<BoxCollider>().size;
+
+        Vector3 newPos = new Vector3(
+            Random.Range(pos.x - size.x / 2, pos.x + size.x / 2), 
+            y, 
+            Random.Range(pos.z - size.z / 2, pos.z + size.z / 2)
+        );
+
+        transform.LookAt(newPos);
+        gameObject.transform.eulerAngles = new Vector3(
+            -90,
+            gameObject.transform.eulerAngles.y,
+            gameObject.transform.eulerAngles.z
+        );
+
+        return newPos;
     }
 }
